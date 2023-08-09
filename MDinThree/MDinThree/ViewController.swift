5d5648c553e461337e5706a9206431ce3e18b3c1
Joshua Mae <86929441+joshua-mae@users.noreply.github.com>
2023-08-08 21:02:54 -0400

    Programmatic UI, more to come tmrw

---
+++ MDinThree/MDinThree/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MDinThree
+//
+//  Created by Joshua Mae on 8/8/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .green
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
